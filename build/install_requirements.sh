d242fca8742b4a750bad03f9cdb5d74268739c22
Rob1in <robin.in34790@gmail.com>
2024-12-19 16:35:54 -0500

    fix path

--- build/install_requirements.sh
+++ build/install_requirements.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+VENV_DIR="build/.build_venv"
 source $VENV_DIR/bin/activate
 wget https://nvidia.box.com/shared/static/i7n40ki3pl2x57vyn4u7e9asyiqlnl7n.whl -O onnxruntime_gpu-1.17.0-cp310-cp310-linux_aarch64.whl
 pip install onnxruntime_gpu-1.17.0-cp310-cp310-linux_aarch64.whl
